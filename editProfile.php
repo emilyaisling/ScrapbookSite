90fcbf7d6af7765b218e46bbeb6a6283fb44a545
emilyaisling <emilybrown26343@gmail.com>
2020-07-19 11:09:18 +0100

    fixed homepage photo issue, user now chooses image of their first initial from preset selection

--- editProfile.php
+++ editProfile.php
@@ -20,39 +20,140 @@ catch (PDOexception $exception)
 if (isset($_POST['submit']))
 {
     $_SESSION['formSet'] = TRUE;
+    $file = '';
 
-    $folder = 'img/';
-    $file = $folder . basename($_FILES['photo']['name']);
-    $fileTmpName = $_FILES['photo']['tmp_name'];
-    $fileSize = $_FILES['photo']['size'];
+    $initial = $_POST['photo'];
+
+    switch ($initial) {
+        case 'a':
+            $file = 'img/a.png';
+            break;
+        
+        case 'b':
+            $file = 'img/b.png';
+            break;
+        
+        case 'c':
+            $file = 'img/c.png';
+            break;
+        
+        case 'd':
+            $file = 'img/d.png';
+            break;
+        
+        case 'e':
+            $file = 'img/e.png';
+            break;
+        
+        case 'f':
+            $file = 'img/f.png';
+            break;
+        
+        case 'g':
+            $file = 'img/g.png';
+            break;
+        
+        case 'h':
+            $file = 'img/h.png';
+            break;
+        
+        case 'i':
+            $file = 'img/h.png';
+            break;
+        
+        case 'j':
+            $file = 'img/h.png';
+            break;
+        
+        case 'k':
+            $file = 'img/k.png';
+            break;
+        
+        case 'l':
+            $file = 'img/l.png';
+            break;
+        
+        case 'm':
+            $file = 'img/m.png';
+            break;
+        
+        case 'n':
+            $file = 'img/n.png';
+            break;
+        
+        case 'o':
+            $file = 'img/o.png';
+            break;
+        
+        case 'p':
+            $file = 'img/p.png';
+            break;
+        
+        case 'q':
+            $file = 'img/q.png';
+            break;
+        
+        case 'r':
+            $file = 'img/r.png';
+            break;
+        
+        case 's':
+            $file = 'img/s.png';
+            break;
+        
+        case 't':
+            $file = 'img/t.png';
+            break;
+        
+        case 'u':
+            $file = 'img/u.png';
+            break;
+        
+        case 'v':
+            $file = 'img/v.png';
+            break;
+        
+        case 'w':
+            $file = 'img/w.png';
+            break;
+        
+        case 'x':
+            $file = 'img/x.png';
+            break;
+        
+        case 'y':
+            $file = 'img/y.png';
+            break;
+        
+        case 'z':
+            $file = 'img/z.png';
+            break;
+        
+        default:
+            $file = '';
+            break;
+    }
+
     $fileType = strtolower(pathinfo($file,PATHINFO_EXTENSION));
-
     $validExtensions = array('jpg', 'jpeg', 'png', 'gif', 'pdf');
 
     if (in_array($fileType, $validExtensions))
     {
-        if ($fileSize < 1000000)
+        if ($stmt = $pdo->prepare("UPDATE users SET bio = ?, photo = ? WHERE id = ?"))
         {
-            if (! file_exists($file))
-            {
-                move_uploaded_file($fileTmpName, $file);
-            }
-            if ($stmt = $pdo->prepare("UPDATE users SET bio = ?, photo = ? WHERE id = ?"))
-            {
-                $stmt->execute(array($_POST['bio'], $file, $_SESSION['id']));
+            $stmt->execute(array($_POST['bio'], $file, $_SESSION['id']));
 
-                if ($stmt = $pdo->prepare("SELECT bio, photo FROM users WHERE id = ?"))
+            if ($stmt = $pdo->prepare("SELECT bio, photo FROM users WHERE id = ?"))
+            {
+                $stmt->execute(array($_SESSION['id']));
+                $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                if ($result)
                 {
-                    $stmt->execute(array($_SESSION['id']));
-                    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+                    $_SESSION['bio'] = $result['bio'];
+                    $_SESSION['image'] = $result['photo'];
 
-                    if ($result)
-                    {
-                        $_SESSION['bio'] = $result['bio'];
-                        $_SESSION['image'] = $result['photo'];
-
-                        header('Location: index.php');
-                    }
+                    header('Location: index.php');
                 }
             }
         }
--- index.php
+++ index.php
@@ -15,12 +15,6 @@ catch (PDOexception $exception)
     exit('Failed to connect to database!');
 }
 
-if (! isset($_SESSION['formSet']))
-{
-    $_SESSION['image'] = '';
-    $_SESSION['bio'] = 'About me...';
-}
-
 if (!isset($_SESSION['loggedin']))
 {
     header('Location: login.html');
@@ -36,6 +30,11 @@ if ($stmt = $pdo->prepare("SELECT bio, photo FROM users WHERE id = ?"))
     {
         $_SESSION['bio'] = $result['bio'];
         $_SESSION['image'] = $result['photo'];
+    }
+    else
+    {
+        $_SESSION['image'] = '';
+        $_SESSION['bio'] = 'About me...';
     }
 }
 ?>
@@ -97,27 +96,49 @@ if ($stmt = $pdo->prepare("SELECT bio, photo FROM users WHERE id = ?"))
                     <div class="bio">
                         <br>
                         <br>
-                        <blockquote><? print $_SESSION['bio'];?></blockquote>
+                        <blockquote><?php print $_SESSION['bio'];?></blockquote>
                     </div>
                 </div>
             </div>
             <br>
             <a href="#edit-form" class="btn red lighten-1 modal-trigger">Edit Profile</a>
             <div class="modal" id="edit-form">
-                <div class="modal-content">
+                <div class="modal-content row">
                     <form action="editProfile.php" method="POST" enctype="multipart/form-data">
-                        <div class="input-field">
+                        <div class="input-field col s12">
                             <textarea id="bio" class="materialize-textarea" data-length="120" name="bio"></textarea>
                             <label for="bio">About you...</label>
                         </div>
-                        <div class="file-field input-field">
-                            <div class="btn red lighten-1">
-                                <span>Change Photo</span>
-                                <input type="file" name="photo">
-                            </div>
-                            <div class="file-path-wrapper">
-                                <input class="file-path validate" type="text">
-                            </div>
+                        <div class="input-field col s12 selector">
+                            <select class="icons" id="photo" name="photo">
+                            <option value="" disabled selected>Choose your initial</option>
+                            <option value="a" data-icon="img/a.png" class="left">A</option>
+                            <option value="b" data-icon="img/b.png" class="left">B</option>
+                            <option value="c" data-icon="img/c.png" class="left">C</option>
+                            <option value="d" data-icon="img/d.png" class="left">D</option>
+                            <option value="e" data-icon="img/e.png" class="left">E</option>
+                            <option value="f" data-icon="img/f.png" class="left">F</option>
+                            <option value="g" data-icon="img/g.png" class="left">G</option>
+                            <option value="h" data-icon="img/h.png" class="left">H</option>
+                            <option value="i" data-icon="img/i.png" class="left">I</option>
+                            <option value="j" data-icon="img/j.png" class="left">J</option>
+                            <option value="k" data-icon="img/k.png" class="left">K</option>
+                            <option value="l" data-icon="img/l.png" class="left">L</option>
+                            <option value="m" data-icon="img/m.png" class="left">M</option>
+                            <option value="n" data-icon="img/n.png" class="left">N</option>
+                            <option value="o" data-icon="img/o.png" class="left">O</option>
+                            <option value="p" data-icon="img/p.png" class="left">P</option>
+                            <option value="q" data-icon="img/q.png" class="left">Q</option>
+                            <option value="r" data-icon="img/r.png" class="left">R</option>
+                            <option value="s" data-icon="img/s.png" class="left">S</option>
+                            <option value="t" data-icon="img/t.png" class="left">T</option>
+                            <option value="u" data-icon="img/u.png" class="left">U</option>
+                            <option value="v" data-icon="img/v.png" class="left">V</option>
+                            <option value="w" data-icon="img/w.png" class="left">W</option>
+                            <option value="x" data-icon="img/x.png" class="left">X</option>
+                            <option value="y" data-icon="img/y.png" class="left">Y</option>
+                            <option value="z" data-icon="img/z.png" class="left">Z</option>
+                            </select>
                         </div>
                         <div class="input-field right">
                             <button type="submit" class="modal-close btn red lighten-1" name="submit">Save changes</button>
@@ -137,7 +158,7 @@ if ($stmt = $pdo->prepare("SELECT bio, photo FROM users WHERE id = ?"))
         $(document).ready(function(){
             $('.modal').modal();
             $('#bio').characterCounter();
-
+            $('select').formSelect();
         });
         let photoSrc = $('.main img').attr('src');
 
