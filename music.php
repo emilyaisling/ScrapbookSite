d1edd295f72bf627a6453d72305cd875f8cfd084
emilyaisling <emilybrown26343@gmail.com>
2020-07-31 12:23:49 +0100

    included header

--- music.php
+++ music.php
@@ -36,46 +36,7 @@ else
 }
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="styles.css">
-        <!-- Font Awsome icons stylesheet-->
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-        <!-- Compiled and minified CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <!--Import Google Icon Font-->
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <title>Scrapbook</title>
-</head>
-<body>   
-    <header>
-        <nav class="nav-wrapper">
-            <div class="container">
-                <a href="index.php" class="brand-logo">Scrapbook</a>
-                <ul class="right">
-                    <li class="hide-on-med-and-down"><a href="index.php" class="valign-wrapper"><?php print htmlspecialchars($_SESSION['username']);?><i class="large material-icons right navcon">account_circle</i><img src="<?php print $_SESSION['image'];?>" class="responsive-img circle"></a></li>
-                    <li class="hide-on-med-and-down"><a href="logout.php">Log out <i class="fas fa-sign-out-alt"></i></a></li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-
-    <div class="sidebar orange lighten-5">
-        <ul>
-            <li class="hide-on-large-only"><a href="index.php" class="valign-wrapper brown-text text-lighten-1"><?php print htmlspecialchars($_SESSION['username']);?><i class="material-icons right navcon">account_circle</i><img src="<?php print $_SESSION['image'];?>" class="responsive-img circle"></a></li>
-            <li class="hide-on-large-only toplink"><a href="logout.php" class="brown-text text-lighten-1">Log out <i class="fas fa-sign-out-alt"></i></a></li>
-            <li><a href="music.php" class="red-text text-lighten-1">Music</a></li>
-            <li><a href="books.php" class="red-text text-lighten-1">Books</a></li>
-            <li><a href="tv.php" class="red-text text-lighten-1">TV</a></li>
-            <li><a href="films.php" class="red-text text-lighten-1">Films</a></li>
-            <li><a href="recipes.php" class="red-text text-lighten-1">Recipes</a></li>
-            <li><a href="journal.php" class="red-text text-lighten-1">Journal</a></li>
-        </ul>
-    </div>
+<?php include('header.php'); ?>
 
     <div class="main">
         <div class="container">
